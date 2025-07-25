cb9a9c26f5e624ca378331f8f8ddb4d070b8e3ae
Anna De Lise <anna_delise@libero.it>
2025-07-25 11:39:37 +0200

    add FilmRows

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import FilmRow from './components/FilmRow'
 import Hero from './components/Hero'
 import MyFooter from './components/MyFooter'
 import MyNav from './components/MyNav'
@@ -9,6 +10,9 @@ function App() {
     <>  
     <MyNav/>  
     <Hero/>
+    <FilmRow query ='Matrix' h4='I titoli del momento' num='5'/>
+    <FilmRow query ='Blade' h4='Perchè ti è piaciuto Matrix' num='6'/>
+    <FilmRow query ='Cube' h4='Continua a guardare' num='2'/>
     <MyFooter/>
     </>
   )
---
+++ src/components/FilmRow.jsx
@@ -0,0 +1,66 @@
+import { Alert, Spinner, Container, Row, Col, Image } from "react-bootstrap";
+import { Component } from "react";
+
+class FilmRow extends Component {
+  state = {
+    isError: false,
+    isLoading: true,
+    films: [],
+  };
+
+  endpoint = "https://www.omdbapi.com/?apikey=";
+  key = "f5942648";
+
+  getFilms = () => {
+   fetch(this.endpoint + this.key + "&s=" + this.props.query)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(`Errore richiesta: ${response.status}`);
+        }
+      })
+      .then((data) => {
+          this.setState({ films: data.Search, isLoading: false });
+      })
+      .catch((err) => {
+        this.setState({ isError: true, isLoading: false });
+        console.error(err);
+      });
+  };
+
+  componentDidMount() {
+    this.getFilms(); 
+  }
+
+  render() {
+    const { isError, isLoading, films } = this.state;
+
+    return (
+       <>
+       <div className="bg-dark p-2">
+        <h4 className="ms-4 text-light">{this.props.h4}</h4>
+        {isError && ( <Alert variant="danger">Impossibile recuperare i film...</Alert>)}
+        {isLoading && <Spinner animation="border" variant="light" />}
+        {!isLoading && !isError && (
+          <Container fluid>
+            <Row className="row-cols-1 row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 px-4 ">
+
+                {/* sennò escono anche i giochi */}
+              {films.slice(0, this.props.num).map((film) => (
+                <Col key={film.imdbID}  className="mb-2 text-center px-1 d-flex flex-column h-100">
+                  <Image fluid
+                    src={film.Poster} alt={`Copertina film: ${film.Title}`} 
+                    style={{ objectFit: 'cover', height: '400px' }} />
+                </Col>
+              ))}
+            </Row>
+          </Container>
+        )}
+        </div>
+      </>
+  );
+}
+}
+
+export default FilmRow;
